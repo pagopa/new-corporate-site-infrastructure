a68c3411d49a5848242fc7b355ab6f49d8ce6332
uolter <walter.traspadini@pagopa.it>
2023-03-16 14:54:07 +0100

    fix repo name in iac role

--- src/init/variables.tf
+++ src/init/variables.tf
@@ -13,7 +13,7 @@ variable "environment" {
 variable "github_repository" {
   type        = string
   description = "This github repository"
-  default     = "pagopa/new-corporate-site-infrastructure.git"
+  default     = "pagopa/new-corporate-site-infrastructure"
 }
 
 
