6076286c48d67067068ea6e05c6a24fec1182c95
Fabio Lisco <12395006+fmlisco@users.noreply.github.com>
2023-03-24 15:00:09 +0100

    not executed twice

--- src/main/log_group_import.sh
+++ src/main/log_group_import.sh
@@ -1,29 +1,36 @@
-service_name="$(terraform output | grep app_runner_service_arn | awk -F\/ '{ print $2 }')"
-service_id="$(terraform output | grep app_runner_service_id | awk -F\" '{ print $2 }')"
+#!/bin/bash
 
-terraform output | grep app_runner_service_id | awk -F\" '{ print $2 }'
+APPRUNNER_LOG="46-apprunner_log_group.tf"
 
-cat <<EOF > 46-apprunner_log_group.tf
-resource "aws_cloudwatch_log_group" "service" {
-  name = "/aws/apprunner/${service_name}/${service_id}/service"
+if [ ! -f "$APPRUNNER_LOG" ]; then
+  service_name="$(terraform output | grep app_runner_service_arn | awk -F\/ '{ print $2 }')"
+  service_id="$(terraform output | grep app_runner_service_id | awk -F\" '{ print $2 }')"
 
-  retention_in_days = var.log_group_policy_retention
+  terraform output | grep app_runner_service_id | awk -F\" '{ print $2 }'
 
-}
+  cat <<EOF > $APPRUNNER_LOG
+  resource "aws_cloudwatch_log_group" "service" {
+    name = "/aws/apprunner/${service_name}/${service_id}/service"
 
-resource "aws_cloudwatch_log_group" "application" {
-  name = "/aws/apprunner/${service_name}/${service_id}/application"
+    retention_in_days = var.log_group_policy_retention
 
-  retention_in_days = var.log_group_policy_retention
+  }
 
-}
+  resource "aws_cloudwatch_log_group" "application" {
+    name = "/aws/apprunner/${service_name}/${service_id}/application"
 
-variable "log_group_policy_retention" {
-  type        = number
-  description = "Retention days policy related to a log group app runner"
-  default     = 30
-}
+    retention_in_days = var.log_group_policy_retention
+
+  }
+
+  variable "log_group_policy_retention" {
+    type        = number
+    description = "Retention days policy related to a log group app runner"
+    default     = 30
+  }
 EOF
 
-./terraform.sh import dev  aws_cloudwatch_log_group.application "/aws/apprunner/${service_name}/${service_id}/application"
-./terraform.sh import dev  aws_cloudwatch_log_group.service "/aws/apprunner/${service_name}/${service_id}/service"
+  ./terraform.sh import dev  aws_cloudwatch_log_group.application "/aws/apprunner/${service_name}/${service_id}/application"
+  ./terraform.sh import dev  aws_cloudwatch_log_group.service "/aws/apprunner/${service_name}/${service_id}/service"
+
+fi
