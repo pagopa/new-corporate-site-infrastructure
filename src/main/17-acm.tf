21ec35c86adf4b3a003f6ab0edca3fb4374d1fbb
Walter Traspadini <walter.traspadini@pagopa.it>
2023-04-06 11:08:44 +0200

    feat: App runner custom domain (#11)

--- src/main/17-acm.tf
+++ src/main/17-acm.tf
@@ -9,12 +9,3 @@ resource "aws_acm_certificate" "website" {
 
   provider = aws.us-east-1
 }
-
-resource "aws_acm_certificate" "cms" {
-  domain_name       = format("cms.%s", keys(var.public_dns_zones)[0])
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
